411e6b96a5c8ed77bba2e57832a5dfc5534065ba
liaddd <liaddd@gmail.com>
2020-07-16 21:35:46 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,119 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Typography, PageHeader, Form, Input, Button, Checkbox } from 'antd';
+const { Title} = Typography
 
 function App() {
+
+  function onFinish(){
+
+  }
+
+  function onFinishFailed(){
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typography>
+        <Title>Harry Porter</Title>
+      </Typography>
+      <Form
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Full Name"
+        name="fullname"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your fullname!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="ID number"
+        name="id_number"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your id number!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Phone"
+        name="phone"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your phone!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="School Name"
+        name="school_name"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your School name!',
+          },
+        ]}
+      >
+        <Input/>
+      </Form.Item>
+
+       <Form.Item
+        label="Class"
+        name="class"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your class!',
+          },
+        ]}
+      >
+        <Input/>
+      </Form.Item>
+
+      <Form.Item
+        label="Class Number"
+        name="class_number"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your class number!',
+          },
+        ]}
+      >
+        <Input/>
+      </Form.Item>
+
+      <Form.Item name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
     </div>
   );
 }
